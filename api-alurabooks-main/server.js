7062b62fbf09762d3f3fbe1647f0db301e727265
MARIA LUISA SILVA OLIVEIRA <malusilvaoliveira14@gmail.com>
2025-04-10 08:38:47 -0300

    Update server.js

--- api-alurabooks-main/server.js
+++ api-alurabooks-main/server.js
@@ -359,5 +359,5 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, (req, res, next) => {
 server.use(router)
 
 server.listen(8000, () => {
-  console.log("API disponível em http://localhost:8000")
+  console.log("Acesse essa API em http://localhost:8000")
 })
